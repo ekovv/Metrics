54c9a8eee08b91bd12416f62b559567c95be4bb3
ekovv <denisovdima2004@yandex.ru>
2023-09-06 23:26:57 +0300

    :tada:iter1

--- internal/server/storage/map_storage.go
+++ internal/server/storage/map_storage.go
@@ -30,10 +30,10 @@ func (s *Storage) Set(name string, value float64) {
 
 func (s *Storage) Inc(name string, value float64) {
 	_, ok := s.m[name]
-	new_value := 0.0
+	newValue := 0.0
 	if ok {
-		new_value = s.m[name] + value
-		s.m[name] = new_value
+		newValue = s.m[name] + value
+		s.m[name] = newValue
 	} else {
 		s.m[name] = value
 	}
