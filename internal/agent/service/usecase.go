a92b1f820d360b50348379685696e1b01f3e3907
ekovv <denisovdima2004@yandex.ru>
2023-09-08 11:41:08 +0300

    :tada:iter2

--- internal/agent/service/usecase.go
+++ internal/agent/service/usecase.go
@@ -1,3 +1,21 @@
 package service
 
-type Service struct{}
+import (
+	"metrics/internal/agent/storage"
+)
+
+//var memStats runtime.MemStats
+//runtime.ReadMemStats(&memStats)
+//
+//fmt.Printf("Общий использованный объем памяти: %d bytes\n", memStats.Alloc)
+
+type Service struct {
+	array storage.Storage
+}
+
+func (a *Service) send() error {
+	//
+	//time.Sleep(10 * time.Second)
+	//req, err := http.NewRequest("POST", "http://example.com/api/"+a.array, nil)
+	return nil
+}
--- internal/agent/storage/list_storage.go
+++ internal/agent/storage/list_storage.go
@@ -1,3 +1,17 @@
 package storage
 
-type storage struct{}
+type Storage struct {
+	metrics []string
+}
+
+type Inter interface {
+	Set(metric string)
+}
+
+func NewStorage(metrics []string) *Storage {
+	return &Storage{metrics: make([]string, 0)}
+}
+
+func (s *Storage) Set(metric string) {
+
+}
