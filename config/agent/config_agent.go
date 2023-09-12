3ccc0dd1a180db72e36e3bf8c28380424830ff96
ekovv <denisovdima2004@yandex.ru>
2023-09-12 17:48:02 +0300

    :tada:iter4

--- config/agent/config_agent.go
+++ config/agent/config_agent.go
@@ -9,7 +9,7 @@ var (
 )
 
 func ParseFlagsAgent() {
-	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
+	flag.StringVar(&FlagRunAddr, "a", "8080", "address and port to run server")
 	flag.IntVar(&FlagIntReportInterval, "r", 10, "interval of send metrics")
 	flag.IntVar(&FlagIntPollInterval, "p", 2, "interval of update metrics")
 	flag.Parse()
