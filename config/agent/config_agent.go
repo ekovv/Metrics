a528b8fe758611c626c91ef8e60cde783f5d615d
ekovv <denisovdima2004@yandex.ru>
2023-09-12 22:38:39 +0300

    :tada:iter5

--- config/agent/config_agent.go
+++ config/agent/config_agent.go
@@ -1,6 +1,10 @@
 package agent
 
-import "flag"
+import (
+	"flag"
+	"os"
+	"strconv"
+)
 
 var (
 	FlagRunAddr           string
@@ -13,4 +17,18 @@ func ParseFlagsAgent() {
 	flag.IntVar(&FlagIntReportInterval, "r", 10, "interval of send metrics")
 	flag.IntVar(&FlagIntPollInterval, "p", 2, "interval of update metrics")
 	flag.Parse()
+	envRunAddr := os.Getenv("ADDRESS")
+	envReport := os.Getenv("REPORT_INTERVAL")
+	envPoll := os.Getenv("POLL_INTERVAL")
+
+	if envRunAddr != "" {
+		FlagRunAddr = envRunAddr
+	}
+	if reportInterval, _ := strconv.Atoi(envReport); reportInterval != 0 {
+		FlagIntReportInterval = reportInterval
+	}
+
+	if pollInterval, _ := strconv.Atoi(envPoll); pollInterval != 0 {
+		FlagIntReportInterval = pollInterval
+	}
 }
--- config/server/config_server.go
+++ config/server/config_server.go
@@ -1,10 +1,17 @@
 package server
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var FlagRunAddr string
 
 func ParseFlagsServer() {
 	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
 	flag.Parse()
+	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
+		FlagRunAddr = envRunAddr
+	}
+
 }
